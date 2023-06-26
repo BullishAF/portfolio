3444027b52b6cbb2e71ae41235a774c02bf36afd
coinwire_us <coinwire.us@gmail.com>
2023-06-26 13:35:17 +0000

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
   // ...
   site: 'https://ryanp.vercel.app',
-  integrations: [sitemap()],
-})
+  integrations: [sitemap()]
+});
